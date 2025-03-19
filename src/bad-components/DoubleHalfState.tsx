2eb1d8f5a1ee272ec5b130bee7d6a645bc3f40bc
gracedonaher <gdonaher@udel.edu>
2025-03-19 17:33:48 -0400

    changing doublehalfstate

--- src/bad-components/DoubleHalfState.tsx
+++ src/bad-components/DoubleHalfState.tsx
@@ -1,7 +1,18 @@
 import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 
 export function DoubleHalfState(): React.JSX.Element {
     const [dhValue, setDhValue] = useState<number>(10);
+
+    // Function to double the value
+    function doubleValue() {
+        setDhValue((prevValue) => prevValue * 2);
+    }
+
+    // Function to halve the value
+    function halveValue() {
+        setDhValue((prevValue) => prevValue * 0.5);
+    }
 
     return (
         <div>
@@ -9,6 +20,8 @@ export function DoubleHalfState(): React.JSX.Element {
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
+            <Button onClick={doubleValue}>Double</Button>
+            <Button onClick={halveValue}>Halve</Button>
         </div>
     );
 }
